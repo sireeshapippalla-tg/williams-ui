eadd21b3752c573907594600e915033fc6bdfe44
KarthikSeelam <Karthik.S@cognitivehealthit.com>
2024-11-25 17:03:51 +0530

    PowerBI

--- src/pages/incidents/PowerBi.js
+++ src/pages/incidents/PowerBi.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const PowerBi = () => {
   return (
-    <div>PowerBi</div>
-  )
-}
+    <div>
+      <h1>Power BI Report</h1>
+      <iframe
+        title="IAssure_Incidents"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=154832ef-c2bb-4439-8c7f-e4d391c77f7b&autoAuth=true&ctid=e0b88051-942d-47a7-9270-8eff3861662a"
+        frameBorder="0"
+        allowFullScreen={true}
+      ></iframe>
+    </div>
+  );
+};
+
 
 export default PowerBi
